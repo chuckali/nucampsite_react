072c170f511dc8aeb742e3d17a1588e70b297a8c
chuckali <charlescusson@gmail.com>
2023-08-09 23:02:35 -0400

    Comments

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,16 +1,19 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-const CampsiteCard = ({campsite}) => {
-    const {image, name} = campsite;
-    return (
+const CampsiteCard = ({ campsite }) => {
+  const { id, image, name } = campsite;
+  return (
+    <Link to={`${id}`}>
       <Card>
         <CardImg width='100%' src={image} alt={name} />
         <CardImgOverlay>
           <CardTitle>{name}</CardTitle>
         </CardImgOverlay>
       </Card>
-    );
-  };
-  
-  export default CampsiteCard;
+    </Link>
+  );
+};
+
+export default CampsiteCard;
