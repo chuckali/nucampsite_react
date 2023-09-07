ad24952f2235b19f569e0e4d33d673551a1379bb
chuckali <charlescusson@gmail.com>
2023-09-07 14:29:06 -0400

    Async Partners

--- src/features/partners/PartnersList.js
+++ src/features/partners/PartnersList.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Partner from './Partner'; // Import the Partner component
 import { selectAllPartners } from './partnersSlice'; // Import the selectAllPartners function
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
+
+
 
 const PartnersList = () => {
   const partners = useSelector(selectAllPartners);
+  const isLoading = useSelector((state) => state.partners.isLoading);
+  const errMsg = useSelector((state) => state.partners.errMsg);
 
-  return (
-    <Col className='mt-4'>
-      {partners.map((partner) => (
-        <div className='d-flex mb-5' key={partner.id}>
-          <Partner partner={partner} />
-        </div>
-      ))}
-    </Col>
-  );
+    return (
+        isLoading ? (
+            <Loading />
+        ) : errMsg ? (
+            <Error errMsg={errMsg} />
+        ) : (
+            <Col className='mt-4'>
+                <Row>
+                    {partners.map((partner) => {
+                        return (
+                            <div className='d-flex mb-5' key={partner.id}>
+                                <Partner partner={partner} />
+                            </div>
+                        );
+                    })}
+                </Row>
+            </Col>
+        )
+    );
 };
 
 export default PartnersList;
